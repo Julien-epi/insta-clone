1780b9267f1dbc53d434e5ad44ec695973f611d1
Julien <julienranson77@gmail.com>
2022-08-11 14:56:09 +0200

    post finish + project finish

--- components/Post.js
+++ components/Post.js
@@ -6,11 +6,86 @@ import {
   HeartIcon,
   PaperAirplaneIcon,
 } from "@heroicons/react/outline";
+
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  serverTimestamp,
+  setDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 import { useSession } from "next-auth/react";
+import { db } from "../firebase";
+import Moment from "react-moment";
+
+import { useEffect, useState } from "react";
 
 function Post({ id, username, userImg, img, caption }) {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
 
-  const {data : session} = useSession();
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  useEffect(
+    () =>
+      setHasLiked(
+        likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+      ),
+    [likes]
+  );
+
+  const likePost = async () => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+        username: session.user.username,
+      });
+    }
+  };
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+
+    const commentToSend = comment;
+    setComment("");
+
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
+
+  // console.log(comments);
 
   return (
     <div className="bg-white my-7 border rounded-sm">
@@ -34,7 +109,13 @@ function Post({ id, username, userImg, img, caption }) {
       {session && (
         <div className="flex justify-between px-4 pt-4">
           <div className="flex space-x-4">
-            <HeartIcon className="btn" />
+
+            { hasLiked ? (
+                <HeartIconFilled onClick={likePost} className="btn text-red-500" />
+              ) : (
+                
+            <HeartIcon onClick={likePost} className="btn" />
+              )}
 
             <ChatIcon className="btn" />
             <PaperAirplaneIcon className="btn" />
@@ -45,10 +126,38 @@ function Post({ id, username, userImg, img, caption }) {
 
       {/* caption */}
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1"> {likes.length} likes</p>
+        )}
         <span className="font-bold mr-1">{username}</span> {caption}
       </p>
 
       {/* comments */}
+
+      {comments.length > 0 && (
+        <div
+          className="ml-10 h-20 overflow-y-scroll
+           scrollbar-thumb-black scrollbar-thin"
+        >
+          {comments.map((comment) => (
+            <div key={comment.id} className="flex items-center space-x-2 mb-3">
+              <img
+                className="h-7 rounded-full"
+                src={comment.data().userImage}
+                alt=""
+              />
+              <p className="text-sm flex-1">
+                <span className="font-bold">{username}</span>{" "}
+                {comment.data().comment}
+              </p>
+
+              <Moment fromNow className="pr-5 text-xs">
+                {comment.data().timestamp?.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
 
       {/* input box */}
       {session && (
@@ -56,10 +165,19 @@ function Post({ id, username, userImg, img, caption }) {
           <EmojiHappyIcon className="h-7" />
           <input
             type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
             placeholder="Add a comment..."
             className="border-none flex-1 focus:ring-0 outline-none"
           />
-          <button className="font-semibold text-blue-400">Post</button>
+          <button
+            type="submit"
+            className="font-semibold text-blue-400"
+            disabled={!comment.trim()}
+            onClick={sendComment}
+          >
+            Post
+          </button>
         </form>
       )}
     </div>
